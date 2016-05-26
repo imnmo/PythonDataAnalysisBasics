5d663c7f308420ca6c5f2aefce4d2b20e92402d7
enomoha <mohamed.imran.noor.mohamed@ericsson.com>
2016-05-26 12:23:19 +0200

    Startup ML

---
+++ CurrencyDataPredictor/EurInrPredictorPlot.py
@@ -0,0 +1,48 @@
+from xml.etree import ElementTree
+
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime as dt
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+with open('eurofxref-hist.xml', 'rt') as f:
+    tree = ElementTree.parse(f)
+
+time=[]
+rate=[]
+TIME='2010-01-04'  #A breif history in time
+for node in tree.iter('{http://www.ecb.int/vocabulary/2002-08-01/eurofxref}Cube'):
+    
+    tmpTime=(node.attrib.get('time'))
+    currency=node.attrib.get('currency')    
+    if None != tmpTime:
+        time.append(node.attrib.get('time'))
+        if tmpTime == TIME: #go until only the specified time
+            break
+    if currency == 'INR':
+        rate.append(node.attrib.get('rate'))
+
+
+print "####################################################"
+#print time
+#print rate
+
+
+rate=map(float,rate)
+
+dates = [dt.datetime.strptime(d,'%Y-%m-%d').date() for d in time]
+#del dates(-1) # cleanup to remove the last unmatching element
+#print x
+#y1 = range(len(x))
+dates.pop()
+print len(dates),len(rate)
+
+
+plt.plot_date(x=dates, y=rate,fmt="r-")
+plt.title("Currency rate INR in Years")
+plt.ylabel("INR")
+plt.grid(True)
+plt.gcf().autofmt_xdate()
+plt.show()
+
---
+++ README.md
@@ -0,0 +1,23 @@
+<!-- the line below needs to be an empty line C: (its because kramdown isnt
+     that smart and dearly wants an empty line before a heading to be able to
+     display it as such, e.g. website) -->
+
+# Machine Learning algorithms implemented in Python
+
+
+# Introduction
+
+This repository is road map for implementating all basic to advancd machine learning algorithms from scratch. 
+
+
+# How to contribute
+
+If you are willing to contribute to the project . Please raise an issue
+
+# License
+
+This project is licensed under the terms of the MIT license.
+
+# Current Progress
+
+Implementing all algorithms from coursera machine learning course
